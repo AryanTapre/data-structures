7195084a0850cc15630196309e87d452e9f57180
Aryan Tapre <taprearyan7@gmail.com>
2023-03-14 21:01:51 +0530

    su sequences

---
+++ ADT_Data_Structures/Update/Recursion/subSequence.cpp
@@ -0,0 +1,33 @@
+// print all subsequences of given string 
+// maths: power set
+// e.g : {a,b} = {"",a,b,ab} 
+
+#include<iostream>
+using namespace std;
+ 
+    void printSubSequences(string str, string output, int i) {
+        if(i >= str.length()) {
+            if(output == "") cout<<"empty string"<<endl;
+            else cout<<output<<endl;
+            return;
+        }
+
+        // include char case:
+        output.push_back(str[i]);
+        printSubSequences(str,output,i+1);
+
+        // exclude char case;
+        output.pop_back();
+        printSubSequences(str,output,i+1);
+    }
+
+int main() {
+ 
+    string str = "ab";
+    string output = "";
+    int i = 0;
+
+    printSubSequences(str,output,i);
+    
+return (0);
+}
