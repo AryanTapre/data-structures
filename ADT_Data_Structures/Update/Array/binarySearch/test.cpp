b690c5cab8ffa9199f855cce72ae3aa0f4884ec7
Aryan Tapre <taprearyan7@gmail.com>
2023-03-19 13:59:35 +0530

    test

--- ADT_Data_Structures/Update/Array/binarySearch/test.cpp
+++ ADT_Data_Structures/Update/Array/binarySearch/test.cpp
@@ -1,13 +1,43 @@
-#include<iostream>
+#include <iostream>
+#include<vector>
 using namespace std;
- 
+
+    void last_occu(vector<int>& arr,int key, int start, int end,int& ans) {
+      // int start = 0;
+      // int end  = arr.size()-1;
+     int mid = (start + end)/2;
+      // int ans = 0;
+
+        if(start > end) return;
+   
+        if(arr[mid] == key) {
+            ans = mid;
+           // start = mid + 1;
+            last_occu(arr,key,mid+1,end,ans);
+        }
+        else if(arr[mid] < key) {
+          //start = mid + 1;
+           last_occu(arr,key,mid+1,end,ans);
+        }
+        else {
+        //  end = mid - 1;
+          last_occu(arr,key,start,mid-1,ans);
+        }
+       // mid = (start + end)/2;
+      
+
+      
+    }
+
+
 int main() {
-    int a=-1;
-    if(a){
-        cout<<"in a";
-    }
-    else {
-        cout<<"not in a";
-    }
-return (0);
+  vector<int> arr {0,2,3,3,3,3,5,5,5,10};
+  int key = 3;
+  int start = 0;
+  int end = arr.size()-1;
+  int mid = (start+end)/2;
+  int ans = 0;
+  last_occu(arr,key,start,end,ans);
+  cout << ans << endl;
+  return 0;
 }
